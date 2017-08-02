78baae124ae6d4fe61aefd364b403be2a6342d71
robstallion <robertefrancis18@gmail.com>
2017-08-02 09:02:10 +0100

    create basic user controller, view and templates #23 #57 #56 #59

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,6 +2,17 @@ defmodule OxleasAdhd.UserController do
   use OxleasAdhd.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    conn
+    |> render("index.html")
   end
+
+  def new(conn, %{"user_type" => user_type}) do
+    conn
+    |> render(String.to_atom(user_type))
+  end
+
+  # def create(conn, params) do
+  #   conn
+  #   |> redirect()
+  # end
 end
