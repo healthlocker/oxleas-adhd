6a2717f27733fcaaa99b8420246c12b079529d71
katbow <katherine.bowler@gmail.com>
2017-08-02 16:53:08 +0100

    Edit new action for medication #15 Loads in changeset and user into the form

--- web/controllers/medication_controller.ex
+++ web/controllers/medication_controller.ex
@@ -1,14 +1,18 @@
 defmodule OxleasAdhd.MedicationController do
   use OxleasAdhd.Web, :controller
+  alias OxleasAdhd.{Medication, User}
 
   def index(conn, _params) do
     conn
     |> render("index.html")
   end
 
-  def new(conn, params) do
+  def new(conn, %{"user_id" => id}) do
+    user = Repo.get!(User, id)
+    changeset = Medication.changeset(%Medication{})
     conn
-    |> render("new.html")
+    |> render("new.html", changeset: changeset, user: user)
+  end
   end
 
   def edit(conn, params) do
