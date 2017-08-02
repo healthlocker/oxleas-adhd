d4e76d4cfe5eaec9b036f1fe8b9391f35f7c77d0
robstallion <robertefrancis18@gmail.com>
2017-08-02 15:47:48 +0100

    creates create function to allow users to be created #57

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,5 +1,6 @@
 defmodule OxleasAdhd.UserController do
   use OxleasAdhd.Web, :controller
+  alias OxleasAdhd.User
 
   def index(conn, _params) do
     conn
@@ -7,12 +8,24 @@ defmodule OxleasAdhd.UserController do
   end
 
   def new(conn, %{"user_type" => user_type}) do
+    changeset = User.changeset_staff(%User{})
     conn
-    |> render(String.to_atom(user_type))
+    |> render(String.to_atom(user_type), changeset: changeset, user_type: user_type)
   end
 
-  # def create(conn, params) do
-  #   conn
-  #   |> redirect()
-  # end
+  def create(conn, %{"user" => user}) do
+    user_type = user["user_type"]
+    changeset = User.changeset_staff(%User{}, user)
+
+    case Repo.insert(changeset) do
+      {:ok, _entry} ->
+        conn
+        |> put_flash(:info, ["User created successfully"])
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, ["User could not be created"])
+        |> render(String.to_atom(user_type), changeset: changeset, user_type: user_type)
+    end
+  end
 end
