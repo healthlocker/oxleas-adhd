320a2799243b36cf1dec3097ae443899c440dc17
katbow <katherine.bowler@gmail.com>
2017-08-02 16:53:34 +0100

    Insert medication in create action #15

--- web/controllers/medication_controller.ex
+++ web/controllers/medication_controller.ex
@@ -13,6 +13,21 @@ defmodule OxleasAdhd.MedicationController do
     conn
     |> render("new.html", changeset: changeset, user: user)
   end
+
+  def create(conn, %{"user_id" => id, "medication" => medication_params}) do
+    user = Repo.get!(User, id)
+    changeset = Medication.changeset(%Medication{}, medication_params)
+      |> Ecto.Changeset.put_change(:user_id, String.to_integer(id))
+    case Repo.insert(changeset) do
+      {:ok, medication} ->
+        conn
+        |> put_flash(:info, "Medication added")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, "Error adding medication")
+        |> render("new.html", changeset: changeset, user: user)
+    end
   end
 
   def edit(conn, params) do
