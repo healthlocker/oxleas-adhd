a065e23040ab248a438b2b163c45c8c33f019c89
katbow <katherine.bowler@gmail.com>
2017-08-03 11:29:42 +0100

    Add timex dependency #16

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule OxleasAdhd.Mixfile do
   def application do
     [mod: {OxleasAdhd, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :timex]]
   end
 
   # Specifies which paths to compile per environment.
@@ -37,6 +37,8 @@ defmodule OxleasAdhd.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
+     {:timex, "~> 3.1"},
+     {:plug, "~> 1.3.5", override: true},
      {:cowboy, "~> 1.0"}]
   end
 
