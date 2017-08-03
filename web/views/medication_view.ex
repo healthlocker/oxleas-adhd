26277bdfa4cdc1bc0ad5dd78a2b9fb11ac069725
katbow <katherine.bowler@gmail.com>
2017-08-03 12:56:01 +0100

    Remove IO.inspect

--- web/views/medication_view.ex
+++ web/views/medication_view.ex
@@ -4,7 +4,6 @@ defmodule OxleasAdhd.MedicationView do
 
   # reformats datetime to DD/MM/YYYY
   def format_date(datetime) do
-    IO.inspect datetime
     datetime
     |> Timex.format!("{0D}/{0M}/{YYYY}")
   end
