12333eb57dc3130d6944e47e3ac426cf373ab47f
robstallion <robertefrancis18@gmail.com>
2017-08-03 20:53:02 +0100

    create extra tests for user controller #59

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -11,11 +11,29 @@ defmodule OxleasAdhd.UserControllerTest do
     password: "password"
   }
 
-  @not_in %{
+  @staff_not_in %{
     user_type: "new_staff",
     first_name: "not",
     last_name: "there",
     job_role: "clinician",
+    email: "not_in@example.com",
+    password: "password"
+  }
+
+  @carer_in_db %{
+    user_type: "new_carer",
+    first_name: "test",
+    last_name: "test",
+    relationship: "parent",
+    email: "carer@example.com",
+    password: "password"
+  }
+
+  @carer_not_in %{
+    user_type: "new_carer",
+    first_name: "not",
+    last_name: "there",
+    relationship: "parent",
     email: "not_in@example.com",
     password: "password"
   }
@@ -33,6 +51,14 @@ defmodule OxleasAdhd.UserControllerTest do
       password: "password"
     } |> Repo.insert!
 
+    %User{
+      first_name: "test",
+      last_name: "test",
+      relationship: "parent",
+      email: "carer@example.com",
+      password: "password"
+    } |> Repo.insert!
+
     :ok
   end
 
@@ -47,13 +73,13 @@ defmodule OxleasAdhd.UserControllerTest do
       assert html_response(conn, 200)
     end
 
-    test "GET /super-admin/users/new", %{conn: conn} do
+    test "GET /super-admin/users/new for staff", %{conn: conn} do
       conn = get conn, "/super-admin/users/new?user_type=new_staff"
       assert html_response(conn, 200)
     end
 
-    test "can enter a user that is not currently in the db", %{conn: conn} do
-      conn = post conn, user_path(conn, :create), user: @not_in
+    test "can enter staff that is not currently in the db", %{conn: conn} do
+      conn = post conn, user_path(conn, :create), user: @staff_not_in
       user = Repo.get_by(User, email: "not_in@example.com")
       assert redirected_to(conn) == user_path(conn, :index)
       assert user
@@ -65,8 +91,26 @@ defmodule OxleasAdhd.UserControllerTest do
       assert get_flash(conn, :error) == "User could not be created"
     end
 
-    test "cannot enter user with invalid attributes", %{conn: conn} do
+    test "cannot enter staff with invalid attributes", %{conn: conn} do
       conn = post conn, user_path(conn, :create), user: @invalid_attrs
+      assert html_response(conn, 200)
+      assert get_flash(conn, :error) == "User could not be created"
+    end
+
+    test "GET /super-admin/users/new for carer", %{conn: conn} do
+      conn = get conn, "/super-admin/users/new?user_type=new_carer"
+      assert html_response(conn, 200)
+    end
+
+    test "can enter carer that is not currently in the db", %{conn: conn} do
+      conn = post conn, user_path(conn, :create), user: @carer_not_in
+      user = Repo.get_by(User, email: "not_in@example.com")
+      assert redirected_to(conn) == user_carer_path(conn, :new, user)
+      assert user
+    end
+
+    test "cannot enter a carer that is already in the db", %{conn: conn} do
+      conn = post conn, user_path(conn, :create), user: @in_db
       assert html_response(conn, 200)
       assert get_flash(conn, :error) == "User could not be created"
     end
