e2349af27c1a4fb8447c3d2b81e060c32986120e
katbow <katherine.bowler@gmail.com>
2017-08-03 11:29:29 +0100

    Create function to format date to dd/mm/yyyy #16

--- web/views/medication_view.ex
+++ web/views/medication_view.ex
@@ -1,3 +1,11 @@
 defmodule OxleasAdhd.MedicationView do
   use OxleasAdhd.Web, :view
+  use Timex
+
+  # reformats datetime to DD/MM/YYYY
+  def format_date(datetime) do
+    IO.inspect datetime
+    datetime
+    |> Timex.format!("{0D}/{0M}/{YYYY}")
+  end
 end
