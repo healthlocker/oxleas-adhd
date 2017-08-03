50567479af7506891a5487c0f8fe304de99d484e
katbow <katherine.bowler@gmail.com>
2017-08-03 08:33:03 +0100

    Add show action to controller and remove index #16

--- web/controllers/medication_controller.ex
+++ web/controllers/medication_controller.ex
@@ -1,14 +1,19 @@
 defmodule OxleasAdhd.MedicationController do
   use OxleasAdhd.Web, :controller
-  alias OxleasAdhd.{Medication, User}
+  alias OxleasAdhd.{Medication, MedicationQuery, User}
 
-  def index(conn, _params) do
+  def show(conn, %{"user_id" => user_id, "id" => id}) do
+    user = Repo.get!(User, user_id)
+    medication = Medication
+                |> MedicationQuery.get_medication_for_user(user_id, id)
+                |> Repo.one
+
     conn
-    |> render("index.html")
+    |> render("show.html", user: user, medication: medication)
   end
 
-  def new(conn, %{"user_id" => id}) do
-    user = Repo.get!(User, id)
+  def new(conn, %{"user_id" => user_id}) do
+    user = Repo.get!(User, user_id)
     changeset = Medication.changeset(%Medication{})
     conn
     |> render("new.html", changeset: changeset, user: user)
