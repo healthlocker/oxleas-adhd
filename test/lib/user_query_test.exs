47e363c998e85dcad44308c3f79838c03bcc9f7c
robstallion <robertefrancis18@gmail.com>
2017-08-04 09:38:04 +0100

    create test for get user by details #59

--- test/lib/user_query_test.exs
+++ test/lib/user_query_test.exs
@@ -30,6 +30,15 @@ defmodule UserQueryTest do
       password: "password"
     } |> Repo.insert!
 
+    %User{
+      id: 1237,
+      first_name: "test",
+      last_name: "test",
+      dob: "01/01/2000",
+      email: "su@example.com",
+      password: "password"
+    } |> Repo.insert!
+
     :ok
   end
 
@@ -49,5 +58,19 @@ defmodule UserQueryTest do
             |> Enum.map(fn(user) -> user.id end)
     expected = [1234]
     refute actual == expected
+  end
+
+  test "get_user_by_details returns user" do
+    map = %{"su_dob" => "01/01/2000", "su_first_name" => "test", "su_last_name" => "test"}
+    actual =
+      User
+      |> UserQuery.get_user_by_details(map)
+      |> Repo.one()
+
+    expected =
+      User
+      |> Repo.get(1237)
+
+    assert actual == expected
   end
 end
