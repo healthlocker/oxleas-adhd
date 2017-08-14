803e08d33577d2d7f44296106e6d21b6fdb2dca5
katbow <katherine.bowler@gmail.com>
2017-08-14 09:39:24 +0100

    Update ClinicianController to take Ecto.Multi #99 Allows the clinicians to be connected to service users and rooms to be created.

--- web/controllers/clinician_controller.ex
+++ web/controllers/clinician_controller.ex
@@ -1,6 +1,6 @@
 defmodule OxleasAdhd.ClinicianController do
   use OxleasAdhd.Web, :controller
-  alias OxleasAdhd.{User, UserQuery, Clinician, ClinicianQuery}
+  alias OxleasAdhd.{User, UserQuery, Clinician, ClinicianQuery, CreateRooms}
 
   def new(conn, %{"user_id" => user_id}) do
     service_user = Repo.get!(User, user_id)
@@ -16,7 +16,7 @@ defmodule OxleasAdhd.ClinicianController do
   def create(conn, %{"user_id" => user_id, "clinician" => clinician_params}) do
     id = user_id |> String.to_integer
     service_user = Repo.get!(User, id)
-    changeset = Clinician.changeset(%Clinician{})
+    # changeset = Clinician.changeset(%Clinician{})
 
     clinician_ids =
       clinician_params
@@ -24,23 +24,23 @@ defmodule OxleasAdhd.ClinicianController do
       |> Enum.reject(fn (selection) ->
         selection == "false"
       end)
+      |> Enum.map(fn id ->
+        String.to_integer(id)
+      end)
 
+    clinicians = make_clinicians(id, clinician_ids)
     query = Clinician |> ClinicianQuery.get_by_user_id(id)
-    case Repo.delete_all(query) do
-      _ ->
-        clinicians = make_clinicians(id, clinician_ids)
-        case Repo.insert_all(Clinician, clinicians) do
-          {n, nil} ->
-            conn
-            |> put_flash(:info, "Staff connected")
-            |> redirect(to: user_path(conn, :index))
-          _err ->
-            conn
-            |> put_flash(:error, "Could not create connection. Please try again.")
-            |> render("new.html", user: service_user, changeset: changeset)
-        end
+    multi = CreateRooms.connect_clinicians_and_create_rooms(service_user, clinician_ids, clinicians, query)
+    case Repo.transaction(multi) do
+      {:ok, _params} ->
+        conn
+        |> put_flash(:info, "Staff connected")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, "Could not create connection. Please try again.")
+        |> render("new.html", user: service_user, changeset: changeset)
     end
-
   end
 
   defp make_clinicians(user_id, clinicians_list) do
@@ -48,7 +48,7 @@ defmodule OxleasAdhd.ClinicianController do
     |> Enum.map(fn(clinician_id) ->
       %{
         caring_id: user_id,
-        clinician_id: String.to_integer(clinician_id)
+        clinician_id: clinician_id
       }
     end)
   end
