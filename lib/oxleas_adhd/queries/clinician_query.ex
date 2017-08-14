35ad645be31a492ecdeb89d606fb2db844e8af25
katbow <katherine.bowler@gmail.com>
2017-08-14 14:35:13 +0100

    Add query to get clinician ids only #99

--- lib/oxleas_adhd/queries/clinician_query.ex
+++ lib/oxleas_adhd/queries/clinician_query.ex
@@ -5,4 +5,10 @@ defmodule OxleasAdhd.ClinicianQuery do
     from c in struct,
     where: c.caring_id == ^user_id
   end
+
+  def get_clinician_ids_for_user(struct, user_id) do
+    from c in struct,
+    where: c.caring_id == ^user_id,
+    select: c.clinician_id
+  end
 end
