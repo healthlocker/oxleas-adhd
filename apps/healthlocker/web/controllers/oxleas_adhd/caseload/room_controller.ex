9268921aec3922802b03ea565d9b1c8337f5b476
katbow <katherine.bowler@gmail.com>
2017-08-15 16:04:59 +0100

    Update caseload/roomcontroller for oxleas #90 #103

--- apps/healthlocker/web/controllers/oxleas_adhd/caseload/room_controller.ex
+++ apps/healthlocker/web/controllers/oxleas_adhd/caseload/room_controller.ex
@@ -1,32 +1,25 @@
 defmodule Healthlocker.OxleasAdhd.Caseload.RoomController do
-  alias Healthlocker.{EPJSUser, Message, Room, User, Slam.ServiceUser}
+  alias Healthlocker.{Message, Room, User, OxleasAdhd.ServiceUser}
   use Healthlocker.Web, :controller
 
   def show(conn, %{"id" => id, "user_id" => user_id}) do
-    if conn.assigns.current_user.user_guid do
-      room = Repo.get((from r in Room, preload: [messages: :user]), id)
-      messages = Repo.all from m in Message,
+    room = Repo.get((from r in Room, preload: [messages: :user]), id)
+
+    messages =
+      Repo.all from m in Message,
       where: m.room_id == ^room.id,
       order_by: [asc: :inserted_at, asc: :id],
       preload: [:user]
 
-      user = Repo.get!(User, user_id)
-      service_user = ServiceUser.for(user)
-      slam_user = ReadOnlyRepo.one(from e in EPJSUser,
-      where: e."Patient_ID" == ^service_user.slam_id)
+    user = Repo.get!(User, user_id)
+    service_user = ServiceUser.for(user)
 
-      conn
-      |> assign(:service_user, service_user)
-      |> assign(:room, room)
-      |> assign(:messages, messages)
-      |> assign(:slam_user, slam_user)
-      |> assign(:user, user)
-      |> assign(:current_user_id, conn.assigns.current_user.id)
-      |> render("show.html")
-    else
-      conn
-      |> put_flash(:error, "Authentication failed")
-      |> redirect(to: page_path(conn, :index))
-    end
+    conn
+    |> assign(:service_user, service_user)
+    |> assign(:room, room)
+    |> assign(:messages, messages)
+    |> assign(:user, user)
+    |> assign(:current_user_id, conn.assigns.current_user.id)
+    |> render("show.html")
   end
 end
