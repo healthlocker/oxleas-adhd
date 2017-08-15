97e0a09f81c8094e908555af7f66cad0e959f9b9
katbow <katherine.bowler@gmail.com>
2017-08-15 15:24:52 +0100

    Update care_team controllers to get service users & care team from new oxleas functions #90 #103

--- apps/healthlocker/web/controllers/care_team/contact_controller.ex
+++ apps/healthlocker/web/controllers/care_team/contact_controller.ex
@@ -1,12 +1,20 @@
 defmodule Healthlocker.CareTeam.ContactController do
   use Healthlocker.Web, :controller
+  alias OxleasAdhd.ClinicianQuery
+  alias Healthlocker.{Clinician, User}
 
   def show(conn, _params) do
-    service_user = Healthlocker.Slam.ServiceUser.for(conn.assigns.current_user)
-
+    service_user = Healthlocker.OxleasAdhd.ServiceUser.for(conn.assigns.current_user)
+    care_team =
+      Clinician
+      |> ClinicianQuery.get_clinician_ids_for_user(service_user.id)
+      |> Repo.all
+      |> Enum.map(fn id ->
+        Repo.get!(User, id)
+      end)
     conn
     |> assign(:service_user, service_user)
-    |> assign(:care_team, Healthlocker.Slam.CareTeam.for(service_user))
+    |> assign(:care_team, care_team)
     |> render("show.html")
   end
 end
