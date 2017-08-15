c948b367267f1e45b5cc48ca9b4c93f9313c3da9
katbow <katherine.bowler@gmail.com>
2017-08-15 12:52:30 +0100

    Add query for getting all patient ids for a clinician #90 #103

--- apps/oxleas_adhd/lib/oxleas_adhd/clinician_query.ex
+++ apps/oxleas_adhd/lib/oxleas_adhd/clinician_query.ex
@@ -5,4 +5,10 @@ defmodule OxleasAdhd.ClinicianQuery do
     from c in struct,
     where: c.caring_id == ^user_id
   end
+
+  def get_patients_for_staff(struct, staff_id) do
+    from c in struct,
+    where: c.clinician_id == ^staff_id,
+    select: c.caring_id
+  end
 end
