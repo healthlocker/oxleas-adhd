97af504ca879e35fe39720ae4621ba63df5edfe9
katbow <katherine.bowler@gmail.com>
2017-08-15 16:16:31 +0100

    Update caseload/usercontroller for oxleas #90 #103

--- apps/healthlocker/web/controllers/oxleas_adhd/caseload/user_controller.ex
+++ apps/healthlocker/web/controllers/oxleas_adhd/caseload/user_controller.ex
@@ -3,58 +3,43 @@ defmodule Healthlocker.OxleasAdhd.Caseload.UserController do
   use Timex
   alias Healthlocker.SleepTracker
 
-  alias Healthlocker.{User, ReadOnlyRepo, EPJSUser, EPJSPatientAddressDetails, Goal, Post, Slam.ServiceUser}
+  alias Healthlocker.{User, Goal, Post, OxleasAdhd.ServiceUser}
 
   def show(conn, %{"id" => id, "section" => section, "date" => date, "shift" => shift}) do
-    if conn.assigns.current_user.user_guid do
-      date = Date.from_iso8601!(date)
-      shifted_date = case shift do
-        "prev" ->
-          Timex.shift(date, days: -7)
-        "next" ->
-          Timex.shift(date, days: 7)
-        end
+    date = Date.from_iso8601!(date)
+    shifted_date = case shift do
+      "prev" ->
+        Timex.shift(date, days: -7)
+      "next" ->
+        Timex.shift(date, days: 7)
+      end
 
-      details = get_details(id, shifted_date)
+    details = get_details(id, shifted_date)
 
-      conn
-      |> render(String.to_atom(section), user: details.user, slam_user: details.slam_user,
-      address: details.address, goals: details.goals, strategies: details.strategies, room: details.room,
-      service_user: details.service_user, sleep_data: details.sleep_data, date: details.date,
-      symptom_data: details.symptom_data, diary_data: details.diary_data,
-      merged_data: details.merged_data)
-    else
-      conn
-      |> put_flash(:error, "Authentication failed")
-      |> redirect(to: page_path(conn, :index))
-    end
+    conn
+    |> render(String.to_atom(section), user: details.user, goals: details.goals,
+    strategies: details.strategies, room: details.room,
+    service_user: details.service_user, sleep_data: details.sleep_data,
+    date: details.date, symptom_data: details.symptom_data,
+    diary_data: details.diary_data, merged_data: details.merged_data)
   end
 
   def show(conn, %{"id" => id, "section" => section}) do
-    if conn.assigns.current_user.user_guid do
-      details = get_details(id, Date.utc_today())
+    details = get_details(id, Date.utc_today())
 
-      conn
-      |> render(String.to_atom(section), user: details.user, slam_user: details.slam_user,
-      address: details.address, goals: details.goals, strategies: details.strategies,
-      room: details.room, service_user: details.service_user, sleep_data: details.sleep_data,
-      date: details.date, symptom_data: details.symptom_data, diary_data: details.diary_data,
-      merged_data: details.merged_data)
-    else
-      conn
-      |> put_flash(:error, "Authentication failed")
-      |> redirect(to: page_path(conn, :index))
-    end
+    conn
+    |> render(String.to_atom(section), user: details.user, goals: details.goals,
+    strategies: details.strategies, room: details.room,
+    service_user: details.service_user, sleep_data: details.sleep_data,
+    date: details.date, symptom_data: details.symptom_data,
+    diary_data: details.diary_data, merged_data: details.merged_data)
   end
 
   defp get_details(id, date) do
     user = Repo.get!(User, id)
     room = Repo.one! assoc(user, :rooms)
     service_user = ServiceUser.for(user)
-    slam_user = ReadOnlyRepo.one(from e in EPJSUser,
-                where: e."Patient_ID" == ^service_user.slam_id)
-    address = ReadOnlyRepo.one(from e in EPJSPatientAddressDetails,
-                    where: e."Patient_ID" == ^service_user.slam_id)
+
     goals = Goal
           |> Goal.get_goals(id)
           |> Repo.all
@@ -73,8 +58,8 @@ defmodule Healthlocker.OxleasAdhd.Caseload.UserController do
     diary_data = Healthlocker.TrackerController.get_diary_data(date_time, service_user.id)
     merged_data = Healthlocker.TrackerController.merge_tracking_data([], sleep_data, symptom_data, diary_data, DateTime.to_naive(date_time))
 
-    %{user: user, room: room, service_user: service_user, slam_user: slam_user,
-    address: address, goals: goals, strategies: strategies, sleep_data: sleep_data,
+    %{user: user, room: room, service_user: service_user, goals: goals,
+    strategies: strategies, sleep_data: sleep_data,
     date: date, symptom_data: symptom_data, diary_data: diary_data,
     merged_data: merged_data}
   end
