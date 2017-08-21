24ed70672cd4ed47365995e6ad59008ff7f984e1
katbow <katherine.bowler@gmail.com>
2017-08-21 16:22:09 +0100

    Add update action to clinician controller #24

--- apps/healthlocker/web/controllers/oxleas_adhd/clinician_controller.ex
+++ apps/healthlocker/web/controllers/oxleas_adhd/clinician_controller.ex
@@ -1,6 +1,6 @@
 defmodule Healthlocker.OxleasAdhd.ClinicianController do
   use Healthlocker.Web, :controller
-  alias Healthlocker.{User, Clinician, OxleasAdhd.CreateRoom}
+  alias Healthlocker.{User, Clinician, Room, OxleasAdhd.CreateRoom, OxleasAdhd.EditRoom}
   alias OxleasAdhd.{ClinicianQuery, UserQuery}
 
   def edit(conn, %{"user_id" => user_id}) do
@@ -12,6 +12,36 @@ defmodule Healthlocker.OxleasAdhd.ClinicianController do
 
     conn
     |> render("edit.html", user: service_user, changeset: changeset, clinicians: clinicians)
+  end
+
+  def update(conn, %{"user_id" => user_id, "clinician" => clinician_params}) do
+    id = user_id |> String.to_integer
+    service_user = Repo.get!(User, id)
+
+    clinician_ids =
+      clinician_params
+      |> Map.values
+      |> Enum.reject(fn (selection) ->
+        selection == "false"
+      end)
+      |> Enum.map(fn id ->
+        String.to_integer(id)
+      end)
+
+    room = Room |> Repo.get_by(name: "service-user-care-team:" <> Integer.to_string(id))
+    clinicians = make_clinicians(id, clinician_ids)
+    query = Clinician |> ClinicianQuery.get_staff_for_service_user(id)
+    multi = EditRoom.connect_clinicians_and_update_rooms(room, clinician_ids, clinicians, query)
+    case Repo.transaction(multi) do
+      {:ok, _params} ->
+        conn
+        |> put_flash(:info, "Staff updated")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, "Could not create connection. Please try again.")
+        |> render("new.html", user: service_user, changeset: changeset)
+    end
   end
 
   def new(conn, %{"user_id" => user_id}) do
@@ -40,8 +70,7 @@ defmodule Healthlocker.OxleasAdhd.ClinicianController do
       end)
 
     clinicians = make_clinicians(id, clinician_ids)
-    query = Clinician |> ClinicianQuery.get_staff_for_service_user(id)
-    multi = CreateRoom.connect_clinicians_and_create_rooms(service_user, clinician_ids, clinicians, query)
+    multi = CreateRoom.connect_clinicians_and_create_rooms(service_user, clinician_ids, clinicians)
     case Repo.transaction(multi) do
       {:ok, _params} ->
         conn
