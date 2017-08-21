8d62c6351713218c8db99b0bf15b672cc2afc0bb
robstallion <robertefrancis18@gmail.com>
2017-08-21 14:35:29 +0100

    remove secret key base to check if it will work

--- apps/healthlocker/config/prod.exs
+++ apps/healthlocker/config/prod.exs
@@ -22,7 +22,7 @@ config :healthlocker, Healthlocker.Endpoint,
   # url: [scheme: "http", host: System.get_env("HEROKU_URL")], # port: 443],
   # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   # cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  # secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
 config :healthlocker, Healthlocker.Repo,
