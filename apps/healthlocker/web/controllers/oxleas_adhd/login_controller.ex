81e2c4515a8f72247651aae54ff4a9b3e7526a3d
Katy Edwards <katy@mindwaveventures.com>
2017-09-05 15:19:23 +0100

    Direct Clinicians to Caseload

--- apps/healthlocker/web/controllers/oxleas_adhd/login_controller.ex
+++ apps/healthlocker/web/controllers/oxleas_adhd/login_controller.ex
@@ -20,7 +20,7 @@ defmodule Healthlocker.OxleasAdhd.LoginController do
           "clinician" ->
             conn
             |> put_flash(:info, "Logged in as clinician")
-            |> redirect(to: page_path(conn, :index))
+            |> redirect(to: page_path(conn, :caseload))
           _ ->
             conn
             |> put_flash(:info, "Welcome to Headscape Focus!")
