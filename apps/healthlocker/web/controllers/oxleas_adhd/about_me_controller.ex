b58d0e1b0b832495e38399ed681a5c2c61abd5f9
robstallion <robertefrancis18@gmail.com>
2018-01-16 12:14:40 +0000

    update new and edit paths in about me controller #30

--- apps/healthlocker/web/controllers/oxleas_adhd/about_me_controller.ex
+++ apps/healthlocker/web/controllers/oxleas_adhd/about_me_controller.ex
@@ -1,13 +1,30 @@
 defmodule Healthlocker.OxleasAdhd.AboutMeController do
+  alias Healthlocker.AboutMe
   use Healthlocker.Web, :controller
 
   def new(conn, params) do
-    conn
-    |> render("new.html")
+    user_id = conn.assigns.current_user.id
+    changeset = AboutMe.changeset(%AboutMe{})
+
+    about_me_query = from am in AboutMe, where: am.user_id == ^user_id
+
+    case Repo.all(about_me_query) do
+      [about_me | _] ->
+        conn
+        |> redirect(to: about_me_path(conn, :edit, about_me))
+      _ ->
+        conn
+        |> render("new.html", changeset: changeset)
+    end
   end
 
-  def edit(conn, params) do
+
+  def edit(conn, %{"id" => about_me_id}) do
+    about_me = Repo.get(AboutMe, about_me_id)
+    changeset = AboutMe.changeset(about_me)
     conn
-    |> render("edit.html")
+    |> render("edit.html", changeset: changeset, about_me: about_me)
+  end
+
   end
 end
