a63b40952c90431dfee71fa781546a7978e8b366
simonLab <simon.labondance@gmail.com>
2018-01-16 13:36:16 +0000

    add super admin in seed, https://github.com/healthlocker/oxleas-adhd/issues/254#issuecomment-357955408

--- apps/healthlocker/priv/repo/user_only_seeds.exs
+++ apps/healthlocker/priv/repo/user_only_seeds.exs
@@ -35,3 +35,15 @@ Repo.insert!(%User{
   data_access: false,
   role: "slam_user"
 })
+
+Repo.insert!(%User{
+  email: "super@admin.com",
+  password_hash: Comeonin.Bcrypt.hashpwsalt("password"),
+  first_name: "Super",
+  last_name: "Admin",
+  phone_number: "07519 283 475",
+  security_question: "Name of first boss?",
+  security_answer: "Betty",
+  data_access: false,
+  role: "super_admin"
+})
