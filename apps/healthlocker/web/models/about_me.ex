8debf067364526fed2c2fa279588649af9f97e47
robstallion <robertefrancis18@gmail.com>
2018-01-16 16:05:13 +0000

    update model to have info about who updated about me and when #30

--- apps/healthlocker/web/models/about_me.ex
+++ apps/healthlocker/web/models/about_me.ex
@@ -10,6 +10,9 @@ defmodule Healthlocker.AboutMe do
     field :important_people, :string
     field :difficulties, :string
     field :strengths, :string
+    field :last_updated_by, :integer
+    field :team_last_update, :naive_datetime
+    field :my_last_update, :naive_datetime
     belongs_to :user, Healthlocker.User
 
     timestamps()
@@ -19,4 +22,16 @@ defmodule Healthlocker.AboutMe do
     struct
     |> cast(params, [:early_life, :health, :events, :hobbies, :school, :important_people, :difficulties, :strengths])
   end
+
+  def user_updated(changeset) do
+    changeset
+    |> put_change(:my_last_update, NaiveDateTime.utc_now())
+  end
+
+  def team_updated(changeset, user_id) do
+    changeset
+    |> put_change(:last_updated_by, user_id)
+    |> put_change(:my_last_update, NaiveDateTime.utc_now())
+  end
 end
+# NaiveDateTime.utc_now()
