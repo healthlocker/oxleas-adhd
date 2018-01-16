a730150d39bcf2045ed0fc8010e13d45727d9f82
robstallion <robertefrancis18@gmail.com>
2018-01-16 12:14:59 +0000

    add create and update paths for about me controller #30

--- apps/healthlocker/web/controllers/oxleas_adhd/about_me_controller.ex
+++ apps/healthlocker/web/controllers/oxleas_adhd/about_me_controller.ex
@@ -18,6 +18,22 @@ defmodule Healthlocker.OxleasAdhd.AboutMeController do
     end
   end
 
+  def create(conn, %{"about_me" => about_me}) do
+    changeset =
+      conn.assigns.current_user
+      |> build_assoc(:about_mes)
+      |> AboutMe.changeset(about_me)
+
+    case Repo.insert(changeset) do
+      {:ok, _entry} ->
+        conn
+        |> put_flash(:info, ["About me saved successfully"])
+        |> redirect(to: toolkit_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> render("new.html", changeset: changeset)
+    end
+  end
 
   def edit(conn, %{"id" => about_me_id}) do
     about_me = Repo.get(AboutMe, about_me_id)
@@ -26,5 +42,18 @@ defmodule Healthlocker.OxleasAdhd.AboutMeController do
     |> render("edit.html", changeset: changeset, about_me: about_me)
   end
 
+  def update(conn, %{"id" => about_me_id, "about_me" => about_me}) do
+    old_about_me = Repo.get(AboutMe, about_me_id)
+    changeset = AboutMe.changeset(old_about_me, about_me)
+
+    case Repo.update(changeset) do
+      {:ok, _entry} ->
+        conn
+        |> put_flash(:info, ["Updated About Me"])
+        |> redirect(to: toolkit_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> render("edit.html", changeset: changeset, about_me: old_about_me)
+    end
   end
 end
--- apps/healthlocker/web/router.ex
+++ apps/healthlocker/web/router.ex
@@ -71,7 +71,7 @@ defmodule Healthlocker.Router do
     resources "/users", UserController do
       resources "/medication", MedicationController, only: [:index, :show]
     end
-    resources "/about-me", AboutMeController, only: [:new, :edit] #, :create, :update
+    resources "/about-me", AboutMeController, only: [:new, :edit, :create, :update]
     scope "/care-team", CareTeam, as: :care_team do
       resources "/rooms", RoomController, only: [:show]
       resources "/contacts", ContactController, only: [:show], singleton: true
