0e5ea0a4a9aa65a6f33c2c1263a3e3e9219536cd
robstallion <robertefrancis18@gmail.com>
2018-01-17 22:52:35 +0000

    update helper functions and remove format_staff_update_info #30

--- apps/healthlocker/web/models/about_me.ex
+++ apps/healthlocker/web/models/about_me.ex
@@ -28,36 +28,34 @@ defmodule Healthlocker.AboutMe do
     |> put_change(:my_last_update, NaiveDateTime.utc_now())
   end
 
-  def team_updated(changeset, user_id) do
+  def team_updated(changeset, su_id, adhd_team_member_id) do
     changeset
-    |> put_change(:last_updated_by, user_id)
-    |> put_change(:my_last_update, NaiveDateTime.utc_now())
+    |> put_change(:user_id, su_id)
+    |> put_change(:last_updated_by, adhd_team_member_id)
+    |> put_change(:team_last_update, NaiveDateTime.utc_now())
   end
 
   # helper functions
-  def format_staff_update_info(about_me, user) do
-    case about_me.last_updated_by do
-      nil ->
-        "No one from your team has updated your about me yet"
-      _   ->
-        team_name_str = format_team_name(user)
-        date_time_str = format_naive_date(about_me.my_last_update)
-        "#{team_name_str} #{date_time_str}"
+  def format_team_name(user) do
+    if user == nil do
+      false
+    else
+      "[ #{user.first_name} #{user.last_name}, #{user.job_role} ]"
     end
   end
 
-  def format_team_name(user) do
-    "[ #{user.first_name} #{user.last_name}, #{user.job_role} ]"
-  end
-
   def format_naive_date(naive_date) do
-    {{year, month, day}, {hour, minute, _}} = NaiveDateTime.to_erl(naive_date)
-    [y, m, d, h, min] =
-      [year, month, day, hour, minute]
-      |> Enum.map(&Integer.to_string/1)
-      |> Enum.map(fn str ->
-        if String.length(str) == 1, do: "0" <> str, else: str
-      end)
-    "[ #{d}/#{m}/#{y} ][ #{h}:#{min} ]"
+    if naive_date == nil do
+      false
+    else
+      {{year, month, day}, {hour, minute, _}} = NaiveDateTime.to_erl(naive_date)
+      [y, m, d, h, min] =
+        [year, month, day, hour, minute]
+        |> Enum.map(&Integer.to_string/1)
+        |> Enum.map(fn str ->
+          if String.length(str) == 1, do: "0" <> str, else: str
+        end)
+      "[ #{d}/#{m}/#{y} ][ #{h}:#{min} ]"
+    end
   end
 end
