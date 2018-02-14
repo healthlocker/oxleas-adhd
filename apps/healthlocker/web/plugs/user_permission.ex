72ab51e3bd53c87ade9265db17f1779d5cbb7426
robstallion <robertefrancis18@gmail.com>
2018-02-14 12:25:36 +0000

    update permissions of teachers #279

--- apps/healthlocker/web/plugs/user_permission.ex
+++ apps/healthlocker/web/plugs/user_permission.ex
@@ -2,13 +2,27 @@ defmodule Healthlocker.Plugs.RequireCorrectUser do
   import Plug.Conn
 
   def redirect_to_home(conn, user_id) do
-    user_id = if is_binary(user_id) do
-      String.to_integer(user_id)
-    else
-      user_id
-    end
+    current_user_id = conn.assigns.current_user.id
+    su = Healthlocker.Repo.get(Healthlocker.User, user_id)
+    user_id =
+      if is_binary(user_id) do
+        String.to_integer(user_id)
+      else
+        user_id
+      end
 
-    if conn.assigns.current_user.id !== user_id do
+    is_service_users_teacher? =
+      if su do
+        su
+        |> Healthlocker.Repo.preload(:teacher)
+        |> Map.get(:teacher)
+        |> Enum.map(fn teacher -> teacher.id end)
+        |> Enum.member?(current_user_id)
+      else
+        false
+      end
+
+    if current_user_id !== user_id && !is_service_users_teacher? do
       conn
       |> Phoenix.Controller.put_flash(:error,  "You cannot access that page")
       |> Phoenix.Controller.redirect(to: Healthlocker.Router.Helpers.page_path(conn, :index))
