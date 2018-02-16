4f385990428ee49ca0e947e83ad15305aa618c37
robstallion <robertefrancis18@gmail.com>
2018-02-16 13:08:44 +0000

    add new show route to make it more clear who a clinician clicks on in the caseload #316

--- apps/healthlocker/web/controllers/oxleas_adhd/caseload/room_controller.ex
+++ apps/healthlocker/web/controllers/oxleas_adhd/caseload/room_controller.ex
@@ -2,6 +2,25 @@ defmodule Healthlocker.OxleasAdhd.Caseload.RoomController do
   alias Healthlocker.{Message, Room, User, OxleasAdhd.ServiceUser}
   use Healthlocker.Web, :controller
 
+  def show(conn, %{"id" => id, "user_id" => user_id, "patient" => patient_id}) do
+    room = Repo.get((from r in Room, preload: [messages: :user]), id)
+
+    from(m in Message, where: m.room_id == ^room.id)
+    |> Repo.update_all(set: [unread: false])
+
+    messages =
+      Repo.all from m in Message,
+      where: m.room_id == ^room.id,
+      order_by: [asc: :inserted_at, asc: :id],
+      preload: [:user]
+
+    user = Repo.get!(User, user_id)
+    service_user = Repo.get!(User, patient_id)
+
+    assign_helper(conn, service_user, room, messages, user)
+    |> render("show.html")
+  end
+
   def show(conn, %{"id" => id, "user_id" => user_id}) do
     room = Repo.get((from r in Room, preload: [messages: :user]), id)
 
@@ -17,12 +36,17 @@ defmodule Healthlocker.OxleasAdhd.Caseload.RoomController do
     user = Repo.get!(User, user_id)
     service_user = ServiceUser.for(user)
 
+    assign_helper(conn, service_user, room, messages, user)
+    |> render("show.html")
+  end
+
+  def assign_helper(conn, service_user, room, messages, user) do
     conn
     |> assign(:service_user, service_user)
     |> assign(:room, room)
     |> assign(:messages, messages)
     |> assign(:user, user)
     |> assign(:current_user_id, conn.assigns.current_user.id)
-    |> render("show.html")
+
   end
 end
