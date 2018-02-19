ba6ba66624c6d2c5660ebc746e65943745142579
robstallion <robertefrancis18@gmail.com>
2018-02-19 20:20:49 +0000

    rename file. update tests

--- apps/healthlocker/test/controllers/oxleas_adhd/caseload/user_controller_test.exs
+++ apps/healthlocker/test/controllers/oxleas_adhd/caseload/user_controller_test.exs
@@ -1,4 +1,4 @@
-defmodule Healthlocker.Caseload.UserControllerTest do
+defmodule Healthlocker.OxleasAdhd.Caseload.UserControllerTest do
   use Healthlocker.ConnCase
 
   alias Healthlocker.{User, Room, UserRoom}
@@ -13,7 +13,7 @@ defmodule Healthlocker.Caseload.UserControllerTest do
         password_hash: Comeonin.Bcrypt.hashpwsalt("password"),
         security_question: "Question?",
         security_answer: "Answer",
-        slam_id: 201
+        role: "service_user"
       } |> Repo.insert
 
       %User{
@@ -74,11 +74,74 @@ defmodule Healthlocker.Caseload.UserControllerTest do
       assert html_response(conn, 200) =~ "Tracking overview"
     end
 
-    test "caseload/users/:id/room/:idroom teacher", %{conn: conn} do
-      user = Repo.get(User, 123_458)
+    test "clinician clicking to chat to a service user's teacher, /caseload/users/:id/rooms/:room_id?patient=:su_id", %{conn: conn} do
+      user = Repo.get(User, 123_456)
+      teacher = Repo.get(User, 123_458)
       room = Repo.get(Room, 2)
-      conn = get conn, caseload_user_room_path(conn, :show, user, room )
-      assert html_response(conn, 200) =~ "Write a message to service user's care team"
+      conn = get conn, caseload_user_room_path(conn, :show, teacher, room, patient: user)
+      assert html_response(conn, 200) =~ "teacher of"
+    end
+  end
+
+  describe "teacher current_user is assigned" do
+    setup do
+      %User{
+        id: 345678,
+        first_name: "My",
+        last_name: "Name",
+        email: "abc@gmail.com",
+        password_hash: Comeonin.Bcrypt.hashpwsalt("password"),
+        security_question: "Question?",
+        security_answer: "Answer",
+        role: "service_user"
+      } |> Repo.insert
+
+      %User{
+        id: 345679,
+        first_name: "Robert",
+        last_name: "MacMurray",
+        email: "robert_macmurray@nhs.co.uk",
+        password_hash: Comeonin.Bcrypt.hashpwsalt("password"),
+        security_question: "Question?",
+        security_answer: "Answer",
+        role: "clinician"
+      } |> Repo.insert
+
+      %User{
+        id: 345670,
+        first_name: "Teacher",
+        last_name: "Teacher",
+        email: "teacher@nhs.co.uk",
+        password_hash: Comeonin.Bcrypt.hashpwsalt("password"),
+        security_question: "Question?",
+        security_answer: "Answer",
+        role: "teacher"
+      } |> Repo.insert
+
+      %Room{
+        id: 1,
+        name: "service-user-care-team:345678"
+      } |> Repo.insert
+
+      %Room{
+        id: 2,
+        name: "teacher-care-team:345670:345678"
+      } |> Repo.insert
+
+      %UserRoom{
+        user_id: 345678,
+        room_id: 1
+      } |> Repo.insert
+
+      {:ok, conn: build_conn() |> assign(:current_user, Repo.get(User, 345670)) }
+    end
+
+    test "caseload/users/:id/room/:idroom teacher", %{conn: conn} do
+      service_user = Repo.get(User, 345678)
+      teacher = Repo.get(User, 345670)
+      room = Repo.get(Room, 2)
+      conn = get conn, caseload_user_room_path(conn, :show, service_user, room )
+      assert html_response(conn, 200) =~ "Write a message to My Name's care team."
     end
   end
 end
